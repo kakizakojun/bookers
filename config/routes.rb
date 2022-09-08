e88cb7c99d24a308315cc9929eb8109e51937ca0
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-08 10:06:33 +0000

    [add]編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/new'
   get 'top' => 'homes#top'
-  get 'books/new' => 'books#new'
   post 'books' => 'books#creat'
   get 'books' => 'books#index'
   
-  get 'edit' => 'books#edit'
   get 'books/:id' => 'books#show',as: 'book'
-
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id' => 'books#update',as:'update_book'
   # resources :books
 
 
