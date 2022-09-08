0f4d2f9c64e149e038933887a2cc894451ff6ef1
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-08 10:56:09 +0000

    [add]Destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'books' => 'books#creat'
   get 'books' => 'books#index'
-  
   get 'books/:id' => 'books#show',as: 'book'
   get 'books/:id/edit' => 'books#edit',as: 'edit_book'
   patch 'books/:id' => 'books#update',as:'update_book'
+  delete 'books/:id' => 'books#destroy',as:'destroy_book'
   # resources :books
 
 
